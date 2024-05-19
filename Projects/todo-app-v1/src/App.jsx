711780f2b119435b58b31a2258d4569158107992
Khush-Shah <khushshah675@gmail.com>
2024-05-19 23:30:40 +0530

    did way too many things in one commit. dont read this

---
+++ Projects/todo-app-v1/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <a href="https://www.google.com" alt = "nahi hai" target = "_blank" rel="noopener noreferrer">
+				<h1>yaha google hai tu kaha hai</h1>
+			</a>
+    </>
+  )
+}
+
+export default App
---
+++ bootstrap/bootstrap/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+function App() {
+
+  return (
+    <>
+      <div>hello</div>
+			<button>1</button>
+			<button>2</button>
+			<button>3</button>
+			<button>4</button>
+			<button>5</button>
+    </>
+  )
+}
+
+export default App
