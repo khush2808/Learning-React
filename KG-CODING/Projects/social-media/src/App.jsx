4298121d66b2350a456a45727cc3d3bace0a126c
Khush-Shah <khushshah675@gmail.com>
2024-05-28 00:35:58 +0530

    social_media_app

--- KG-CODING/Projects/social-media/src/App.jsx
+++ KG-CODING/Projects/social-media/src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-
+import Header from './components/Header'
+import "bootstrap/dist/css/bootstrap.min.css"
+import Footer from './components/Footer'
 function App() {
   return (
     <>
-      
+		<Header></Header>
+		<Footer></Footer>
     </>
   )
 }
