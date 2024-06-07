5fc51ed38621dc28d4a417d4b81a09a544488dda
Khush-Shah <khushshah675@gmail.com>
2024-06-07 22:51:25 +0530

    started learning react again

--- KG-CODING/Projects/calculator/src/App.js
+++ KG-CODING/Projects/calculator/src/App.js
@@ -1,13 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => increment(), 1000);
+console.log("rerend")
+    return () => clearInterval(interval);
+  }, [count]);
+
   return (
-		<div className="App">
-			<h1>Calculator</h1>
-			Display
-		</div>
-	);
+    <div className="App">
+      <p>Count: {count}</p>
+    </div>
+  );
 }
 
 export default App;
--- KG-CODING/Projects/social-media/src/App.jsx
+++ KG-CODING/Projects/social-media/src/App.jsx
@@ -3,9 +3,11 @@ import './App.css'
 import Header from './components/Header'
 import "bootstrap/dist/css/bootstrap.min.css"
 import Footer from './components/Footer'
+import Sidebar from './components/Sidebar'
 function App() {
   return (
     <>
+		<Sidebar></Sidebar>
 		<Header></Header>
 		<Footer></Footer>
     </>
